78334aafcbaf5fdbaf460a8050c4d52be9adb0f8
deconroy <deconroy@me>
2015-12-12 19:30:00 +1100

    using $location

--- js/controllers/Main.js
+++ js/controllers/Main.js
@@ -1,12 +1,12 @@
-cheese.controller('Main', ['$scope', function($scope) { 
+cheese.controller('Main', ['$scope', '$location', 'Common', function($scope, $location, Common) { 
 	//objects for reuse
+	$scope.hash = $location.url().substring(1);
 	$scope.pages = {
 		home: new Page('home', 'Home', true),
 		services: new Page('services', 'Services'),
 		contact: new Page('contact', 'Contact')
 	};
 	$scope.isCollapsed = true;
-	$scope.hash = getHash();
 	if (!$scope.pages[$scope.hash]) {
 		$scope.current_page = $scope.default_page;
 	} else {
@@ -26,9 +26,9 @@ cheese.controller('Main', ['$scope', function($scope) {
 		if (defaultPage) $scope.default_page = this;
 	}
 }]);
-function getHash() {
-	return window.location.hash.length > 1 ? window.location.hash.substring(1) : 'home';
-}
+// function getHash() {
+// 	return window.location.hash.length > 1 ? window.location.hash.substring(1) : 'home';
+// }
 function getPageContent(page) {
 	return page.title + ' content';
 }
