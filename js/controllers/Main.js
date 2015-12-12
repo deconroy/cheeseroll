e00a2ee84b87c7ffbd3899fa2da7e297ffb9e004
deconroy <deconroy@me>
2015-12-12 18:12:38 +1100

    Made a default page

--- js/controllers/Main.js
+++ js/controllers/Main.js
@@ -1,26 +1,34 @@
 cheese.controller('Main', ['$scope', function($scope) { 
- 	//objects for reuse
-  $scope.pages = {
-  	home: {
-		title: 'Home',
-		link: '#home'
-  	},
-  	services: {
-  		title: 'Services',
-  		link: '#services'
-  	},
-  	contact: {
-  		title: 'Contact',
-  		link: '#contact'
-  	}
-  };
-  $scope.isCollapsed = true;
+	//objects for reuse
+	$scope.pages = {
+		home: new Page('home', 'Home', true),
+		services: new Page('services', 'Services'),
+		contact: new Page('contact', 'Contact')
+	};
+	$scope.isCollapsed = true;
+	$scope.hash = getHash();
+	if (!$scope.pages[$scope.hash]) {
+		$scope.current_page = $scope.default_page;
+	} else {
+		$scope.current_page = $scope.pages[$scope.hash];
+	}
+	$scope.page_body = getPageContent($scope.current_page);
 
-  //init page
-  $scope.location = window.location.hash.length > 1 ? window.location.hash.substring(1) : 'home';
-  $scope.current_page = $scope.pages[$scope.location];
-
-  $scope.goToPage = function(page) {
-    $scope.current_page = page;
-  }
+	$scope.goToPage = function(page) {
+		$scope.current_page = page;
+		$scope.isCollapsed = !$scope.isCollapsed;
+		$scope.page_body = getPageContent($scope.current_page);
+	}
+	function Page(id, display, defaultPage) {
+		this.id = id;
+		this.link = '#' + this.id;
+		this.title = display;
+		if (defaultPage) $scope.default_page = this;
+	}
 }]);
+function getHash() {
+	return window.location.hash.length > 1 ? window.location.hash.substring(1) : 'home';
+}
+function getPageContent(page) {
+	return page.title + ' content';
+}
